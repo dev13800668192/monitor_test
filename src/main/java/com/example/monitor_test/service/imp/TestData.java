1256aa344d27c8b2cf8a15bfa420f7087d1e57ea
dev13800668192 <2484431682@qq.com>
2020-08-19 23:15:35 +0800

    修改模拟数据元

--- src/main/java/com/example/monitor_test/service/imp/TestData.java
+++ src/main/java/com/example/monitor_test/service/imp/TestData.java
@@ -19,20 +19,18 @@ public class TestData {
     @Scheduled(cron = "0/5 * * * * ?")
     public void test() {
         Long timestamp = System.currentTimeMillis();
-        String url = "http://10.0.2.148:8080/api/monitor/client/pushData";
-        String jsonParam = "{\n" +
-                "    \"data\":{\n" +
+        String url = "http://localhost:8080/api/monitor/client/pushData";
+        String jsonParam = "{" +
                 "        \"hostname\":\"DESKTOP-VJKTLSC\",\n" +
-                "        \"ip\":\"10.0.2.2540\",\n" +
+                "        \"ip\":\"10.0.2.254\",\n" +
                 "        \"cpu\":\"72.5\",\n" +
                 "        \"memory\":\"56\",\n" +
                 "        \"gpu\":\"87\",\n" +
                 "        \"fps\":\"60\",\n" +
                 "        \"hardDisk\":\"42.25\",\n" +
                 "        \"io\":\"52.15\",\n" +
-                "        \"updateTime\":\""+timestamp.toString()+"\"\n" +
-                "    }\n" +
-                "}";
+                "        \"updateTime\":"+timestamp+"\n" +
+                "    }" ;
         String s = httpClientUtil.doPostJson(url, jsonParam, null);
         System.out.println(jsonParam);
         System.out.println(s);
--- src/test/java/com/example/monitor_test/MonitorTestApplicationTests.java
+++ src/test/java/com/example/monitor_test/MonitorTestApplicationTests.java
@@ -3,11 +3,22 @@ package com.example.monitor_test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class MonitorTestApplicationTests {
 
     @Test
     void contextLoads() {
+
+//        List<String> list =new ArrayList<>();
+//        List<String> list2 =new ArrayList<>();
+//        list2.add("string");
+//        if (list2.get(0)==list.get(0)){
+//            System.out.printf("true");
+//        }else
+//            System.out.println("false");
     }
 
 }
